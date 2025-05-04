23b5f54c5b11252ecb8250308bf519eeafe4b6dc
wemhack <99555303+wemhack@users.noreply.github.com>
2025-05-04 17:38:58 -0500

    Change

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,19 +1,29 @@
 import { useMemo } from "react";
+import { tipOptions } from "../data/tipOptions";
+import { MenuItem } from "../types";
 import { usePropina } from "../stores/propinaStore";
 import { Collapse, IconButton, List, ListItem } from "@mui/material";
 import { TransitionGroup } from "react-transition-group";
 import { Delete } from "@mui/icons-material";
-import Menu from "./Menu";
-import Propina from "./Propina";
-import Totales from "./Totales";
-import { Transition, TransitionChild } from "@headlessui/react";
 
 export default function Main() {
 
-  const { consumoItems, tip, deleteItem, saveOrden } = usePropina()
+  const { menuItems, consumoItems, tip, addConsumoItem, deleteItem, saveOrden, setTip } = usePropina()
 
   const consumoLength = useMemo(() => consumoItems.length > 0, [consumoItems])
 
+  const subTotal = useMemo<number>(
+    () => consumoItems.reduce((total: number, item: MenuItem) => total + (item.price * item.quantity), 0), [consumoItems]
+  )
+  const propina = useMemo(() => subTotal * tip, [consumoItems, tip])
+  const totalPago = useMemo(() => subTotal + propina, [consumoItems, tip])
+
+  const formatCurrency = (quantity: MenuItem['price']) => {
+    return new Intl.NumberFormat('en-US', {
+      style: "currency",
+      currency: 'USD'
+    }).format(quantity)
+  }
 
   return (
     <main>
@@ -30,7 +40,6 @@ export default function Main() {
               onClick={() => addConsumoItem(plato)}
             >
               <p className="text-xl">{plato.name}</p>
-
               <p className="text-xl font-bold">${plato.price}</p>
             </button>
           ))}
@@ -41,26 +50,61 @@ export default function Main() {
           {consumoLength ? (
             <>
               <h2 className="text-3xl font-bold text-center py-8">Consumo</h2>
+
+              <List>
+                <TransitionGroup>
+                  {consumoItems.map(item => (
+                    <Collapse key={item.id}>
+                      <ListItem
+                        secondaryAction={
+                          <IconButton
+                            edge="end"
+                            aria-label="delete"
+                            title="Delete"
+                            onClick={() => deleteItem(item.id)}
+                          >
+                            <Delete sx={{color: 'red'}} />
+                          </IconButton>
+                        }
+                      >
+                        <div>
+                          <p className="text-xl">{item.name} - ${item.price}</p>
+                          <p className="font-bold text-[18px]">Cantidad: {item.quantity} - ${item.quantity * item.price}</p>
+                        </div>
+                      </ListItem>
+                    </Collapse>
+                  ))}
+                </TransitionGroup>
+              </List>
+
               <div>
-                {consumoItems.map(item => (
-                  <div key={item.id} className="flex items-end justify-between py-5 border-b border-gray-300">
-                    <div>
-                      <p className="text-xl">{item.name} - ${item.price}</p>
-                      <p className="font-bold text-[18px]">Cantidad: {item.quantity} - ${item.quantity * item.price}</p>
-                    </div>
-                    <button
-                      className="size-8 rounded-full bg-red-500 font-bold text-white cursor-pointer"
-                      onClick={() => deleteItem(item.id)}
-                    >
-                      X
-                    </button>
-                  </div>
-                ))}
+
               </div>
 
-              <Propina />
+              <div>
+                <h3 className="text-2xl font-bold pt-8 pb-2">Propina:</h3>
+                <form className="*:text-[18px] flex flex-col">
+                  {tipOptions.map(tip => (
+                    <div key={tip.id}>
+                      <label htmlFor={tip.id}>{tip.label} </label>
+                      <input
+                        type="radio"
+                        name="tip"
+                        id={tip.id}
+                        value={tip.value}
+                        onChange={setTip}
+                      />
+                    </div>
+                  ))}
+                </form>
+              </div>
 
-              <Totales />
+              <div>
+                <h3 className="text-2xl font-bold pt-8">Totales y Propina:</h3>
+                <p className="text-[18px] py-1">Subtotal a pagar: {formatCurrency(subTotal)} </p>
+                <p className="text-[18px] py-1">Propina: {formatCurrency(propina)}</p>
+                <p className="text-[18px] py-1">Total a pagar: {formatCurrency(totalPago)}</p>
+              </div>
 
               <button
                 className="w-full text-center uppercase font-bold text-white bg-neutral-800 hover:bg-neutral-700 cursor-pointer rounded-md p-4 text-xl mt-8 disabled:opacity-20 disabled:bg-neutral-800 disabled:cursor-auto"
@@ -69,8 +113,10 @@ export default function Main() {
               >
                 Guardar Orden
               </button>
-            </div>
-          </Transition>
+            </>
+          ) : (
+            <p className="text-xl font-extralight text-center py-8">La ordén está vacia</p>
+          )}
 
         </div>
       </div>
